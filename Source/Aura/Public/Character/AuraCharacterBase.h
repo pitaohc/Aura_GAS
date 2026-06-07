43fd54abe6133e845ac28962dcb27c813b1f7a99
pitaohc <36119151+pitaohc@users.noreply.github.com>
2026-06-07 20:04:15 +0800

    dev/2-6-Player-and-Enemy-Characters (#16)
    * 删除Tick和SetupPlayerInputComponenth函数实现
    
    * 更新目录结构
    
    1. 增加Aura和Enemy两个Character
    2. 修复AuraCharacterBase中的头文件目录

---
+++ Source/Aura/Public/Character/AuraCharacterBase.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "AuraCharacterBase.generated.h"
+
+UCLASS()
+class AURA_API AAuraCharacterBase : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AAuraCharacterBase();
+
+protected:
+
+	virtual void BeginPlay() override;
+
+public:	
+
+
+};
