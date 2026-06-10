d5a98a4f7d66105f1c63d9da1ff44ae376a53bd2
pitao <pitaohc@qq.com>
2026-06-10 09:03:21 +0800

    Resolve 2-10 Aura Player Controller

--- Source/Aura/Public/Player/AuraPlayerController.h
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -6,6 +6,7 @@
 #include "GameFramework/PlayerController.h"
 #include "AuraPlayerController.generated.h"
 
+class UInputMappingContext;
 /**
  * 
  */
@@ -13,5 +14,12 @@ UCLASS()
 class AURA_API AAuraPlayerController : public APlayerController
 {
 	GENERATED_BODY()
+public:
+	AAuraPlayerController();
+protected:
+	virtual void BeginPlay() override; // 不允许外部调用，但允许继承
 	
+	UPROPERTY(EditAnywhere, category = "Input")
+	TObjectPtr<UInputMappingContext> AuraContext;
+
 };
