9139dd9632a0ae3d7fe2a6776af5275752f56954
pitaohc <36119151+pitaohc@users.noreply.github.com>
2026-06-15 12:35:51 +0800

    Resolve 3-18 The Player State (#37)
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    
    ## Summary by CodeRabbit
    
    ## 发版说明
    
    * **新增功能**
      * 实现了玩家状态网络同步机制，优化了多人游戏中的数据更新频率。
      * 更新了游戏模式和玩家状态相关的蓝图配置。
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

---
+++ Source/Aura/Private/Player/AuraPlayerState.cpp
@@ -0,0 +1,11 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/AuraPlayerState.h"
+
+#include "Aura/Aura.h"
+
+AAuraPlayerState::AAuraPlayerState()
+{
+	NetUpdateFrequency = AURA_PLAYER_STATE_NET_UPDATE_FREQUENCY;
+}
