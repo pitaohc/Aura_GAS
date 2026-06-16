6f1a9ae06d47d91deae741f52f91f4a3d21f7cd1
pitaohc <36119151+pitaohc@users.noreply.github.com>
2026-06-16 21:55:38 +0800

    Resolve 4-25 Health and Mana (#57)
    1. 创建成员变量
    2. 设置Uproperty 通常包含 分类，回调函数，蓝图访问
    3. 修改回调函数
    4. 注册网络同步
    
    Close #42 
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    
    ## Summary by CodeRabbit
    
    ## 发版说明
    
    * **新功能**
      * 实现了生命值和法力值属性系统，支持多人游戏中的实时同步。
      * 添加了生命值、最大生命值、法力值及最大法力值的网络复制机制，确保所有玩家能够正确接收属性变化。
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
+++ Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
@@ -14,4 +14,25 @@ class AURA_API UAuraAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
 
+public:
+	UAuraAttributeSet();
+	virtual void GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const override;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Vital Attributes", ReplicatedUsing= OnRep_Health)
+	FGameplayAttributeData Health;
+	UFUNCTION()
+	void OnRep_Health(const FGameplayAttributeData& OldHealth) const;
+	UPROPERTY(BlueprintReadOnly, Category = "Vital Attributes", ReplicatedUsing= OnRep_MaxHealth)
+	FGameplayAttributeData MaxHealth;
+	UFUNCTION()
+	void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Vital Attributes", ReplicatedUsing= OnRep_Mana)
+	FGameplayAttributeData Mana;
+	UFUNCTION()
+	void OnRep_Mana(const FGameplayAttributeData& OldMana) const;
+	UPROPERTY(BlueprintReadOnly, Category = "Vital Attributes", ReplicatedUsing= OnRep_MaxMana)
+	FGameplayAttributeData MaxMana;
+	UFUNCTION()
+	void OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const;
 };
