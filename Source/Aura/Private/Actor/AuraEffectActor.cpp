13d6543afb0167d2480b0b8682d2f3dea13a31ab
pitaohc <36119151+pitaohc@users.noreply.github.com>
2026-06-18 08:48:06 +0800

    Resolve 4-27 Effect Actor (#60)
    完成血瓶的制作。
    当进入碰撞范围后，或获得对应Actor的AS。尝试修改血量。
    Close #44 
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    
    ## Summary by CodeRabbit
    
    ## 发布说明
    
    * **新增功能**
      * 新增效果演员系统，玩家与其接触时生命值会增加，效果演员随即消失。
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

---
+++ Source/Aura/Private/Actor/AuraEffectActor.cpp
@@ -0,0 +1,48 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Actor/AuraEffectActor.h"
+
+#include "AbilitySystemComponent.h"
+#include "AbilitySystemInterface.h"
+#include "AbilitySystem/AuraAttributeSet.h"
+#include "Components/SphereComponent.h"
+
+
+AAuraEffectActor::AAuraEffectActor()
+{
+
+	PrimaryActorTick.bCanEverTick = false;
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	SetRootComponent(Mesh);
+
+	Sphere = CreateDefaultSubobject<USphereComponent>(TEXT("Sphere"));
+	Sphere->SetupAttachment(GetRootComponent());
+}
+
+
+void AAuraEffectActor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	Sphere->OnComponentBeginOverlap.AddDynamic(this, &AAuraEffectActor::OnOverlap);
+	Sphere->OnComponentEndOverlap.AddDynamic(this, &AAuraEffectActor::EndOverlap);
+}
+
+void AAuraEffectActor::OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	// TODO: 这是一段测试代码，未来将会移除
+	if (IAbilitySystemInterface* ASI = Cast<IAbilitySystemInterface>(OtherActor))
+	{
+		UAbilitySystemComponent* ASC = ASI->GetAbilitySystemComponent();
+		const UAuraAttributeSet* AS = Cast<UAuraAttributeSet>(ASC->GetAttributeSet(UAuraAttributeSet::StaticClass()));
+		UAuraAttributeSet*       MutableAS = const_cast<UAuraAttributeSet*>(AS);
+
+		MutableAS->SetHealth(MutableAS->GetHealth() + 25.0f);
+		Destroy();
+	}
+
+}
+
+void AAuraEffectActor::EndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex) {}
