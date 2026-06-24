f8358c9b7566523350a5a78367286f8bf5a2e44b
pitao <pitaohc@qq.com>
2026-06-24 18:54:48 +0800

    feat: implement mana deduction on health increase in AuraEffectActor

--- Source/Aura/Private/Actor/AuraEffectActor.cpp
+++ Source/Aura/Private/Actor/AuraEffectActor.cpp
@@ -1,13 +1,11 @@
 ﻿// Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "Actor/AuraEffectActor.h"
 
 #include "AbilitySystemComponent.h"
 #include "AbilitySystemInterface.h"
 #include "AbilitySystem/AuraAttributeSet.h"
 #include "Components/SphereComponent.h"
-
 
 AAuraEffectActor::AAuraEffectActor()
 {
@@ -21,7 +19,6 @@ AAuraEffectActor::AAuraEffectActor()
 	Sphere->SetupAttachment(GetRootComponent());
 }
 
-
 void AAuraEffectActor::BeginPlay()
 {
 	Super::BeginPlay();
@@ -30,19 +27,27 @@ void AAuraEffectActor::BeginPlay()
 	Sphere->OnComponentEndOverlap.AddDynamic(this, &AAuraEffectActor::EndOverlap);
 }
 
-void AAuraEffectActor::OnOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+void AAuraEffectActor::OnOverlap(UPrimitiveComponent* OverlappedComponent,
+	AActor*											  OtherActor,
+	UPrimitiveComponent*							  OtherComp,
+	int32											  OtherBodyIndex,
+	bool											  bFromSweep,
+	const FHitResult&								  SweepResult)
 {
 	// TODO: 这是一段测试代码，未来将会移除
 	if (IAbilitySystemInterface* ASI = Cast<IAbilitySystemInterface>(OtherActor))
 	{
 		UAbilitySystemComponent* ASC = ASI->GetAbilitySystemComponent();
 		const UAuraAttributeSet* AS = Cast<UAuraAttributeSet>(ASC->GetAttributeSet(UAuraAttributeSet::StaticClass()));
-		UAuraAttributeSet*       MutableAS = const_cast<UAuraAttributeSet*>(AS);
+		UAuraAttributeSet*		 MutableAS = const_cast<UAuraAttributeSet*>(AS);
 
 		MutableAS->SetHealth(MutableAS->GetHealth() + 25.0f);
+		MutableAS->SetMana(MutableAS->GetMana() - 25.0f);
 		Destroy();
 	}
-
 }
 
-void AAuraEffectActor::EndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex) {}
+void AAuraEffectActor::EndOverlap(
+	UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+}
