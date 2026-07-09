f1cffcba6cc523eb37cf978270e775498b7ae7a5
pitao <pitaohc@qq.com>
2026-07-09 13:00:06 +0800

    Resolve 7-55 Get All Asset Tags
    1. 在回调中遍历并输出Tag名称

--- Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
@@ -11,5 +11,12 @@ void UAuraAbilitySystemComponent::EffectApplied(UAbilitySystemComponent* Target,
 	const FGameplayEffectSpec&											 SpecApplied,
 	FActiveGameplayEffectHandle											 ActiveHandle) const
 {
-	GEngine->AddOnScreenDebugMessage(1, 8.0f, FColor::Green, FString("Effect Applied!"));
+	// GEngine->AddOnScreenDebugMessage(1, 8.0f, FColor::Green, FString("Effect Applied!"));
+	FGameplayTagContainer TagContainer;
+	SpecApplied.GetAllAssetTags(TagContainer);
+	for (const FGameplayTag& Tag : TagContainer)
+	{
+		const FString Msg = FString::Printf(TEXT("GE Tag: %s"), *Tag.ToString());
+		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Green, Msg);
+	}
 }
