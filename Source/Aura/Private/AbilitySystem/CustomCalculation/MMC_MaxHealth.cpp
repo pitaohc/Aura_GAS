225037294f6d24b2a0208fabe01b4b659cab22fd
pitao <pitaohc@qq.com>
2026-07-22 13:03:36 +0800

    test

---
+++ Source/Aura/Private/AbilitySystem/CustomCalculation/MMC_MaxHealth.cpp
@@ -0,0 +1,10 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "AbilitySystem/CustomCalculation/MMC_MaxHealth.h"
+
+UMMC_MaxHealth::UMMC_MaxHealth() {}
+
+float UMMC_MaxHealth::CalculateBaseMagnitude_Implementation(const FGameplayEffectSpec& Spec) const
+{
+	return Super::CalculateBaseMagnitude_Implementation(Spec);
+}
---
+++ Source/Aura/Public/AbilitySystem/CustomCalculation/MMC_MaxHealth.h
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayModMagnitudeCalculation.h"
+#include "MMC_MaxHealth.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class AURA_API UMMC_MaxHealth : public UGameplayModMagnitudeCalculation
+{
+	GENERATED_BODY()
+
+public:
+	UMMC_MaxHealth();
+
+	virtual float CalculateBaseMagnitude_Implementation(const FGameplayEffectSpec& Spec) const override;
+
+private:
+	UPROPERTY(EditAnywhere)
+	float BaseValue = 0.0f;
+	UPROPERTY(EditAnywhere)
+	float LevelScale = 1.0f;
+	UPROPERTY(EditAnywhere)
+	float AttributeScale = 1.0f;
+	UPROPERTY(EditAnywhere)
+	FGameplayEffectAttributeCaptureDefinition AttributeDef;
+};
