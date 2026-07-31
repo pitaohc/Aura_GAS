7a3bbc1c48442eb6a84be77000b3d92710974f20
pitao <pitaohc@qq.com>
2026-08-01 02:22:20 +0800

    Resolve 9-86 Gameplay Tags Singleton
    使用单例实现AuraGameplayTags

---
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class AURA_API FAuraGameplayTags
+{
+public:
+	static FAuraGameplayTags& Get()
+	{
+		static FAuraGameplayTags instance;
+		return instance;
+	}
+	static void                     InitializeNativeGameplayTags();
+	// 禁止拷贝和移动
+	FAuraGameplayTags(const FAuraGameplayTags&) = delete;
+	FAuraGameplayTags& operator=(const FAuraGameplayTags&) = delete;
+	FAuraGameplayTags(FAuraGameplayTags&&) = delete;
+	FAuraGameplayTags& operator=(FAuraGameplayTags&&) = delete;
+private:
+	static FAuraGameplayTags GameplayTags;
+	FAuraGameplayTags() = default;
+	~FAuraGameplayTags() = default;
+};
