04ae20d82bb9d7469d4aae3eec4c11f2c7cf6c73
pitao <pitaohc@qq.com>
2026-08-03 21:12:53 +0800

    Resolve 9-90 Attribute Menu Widget Controller

---
+++ Source/Aura/Private/UI/Controller/AttributeMenuWidgetController.cpp
@@ -0,0 +1,13 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UI/Controller/AttributeMenuWidgetController.h"
+
+void UAttributeMenuWidgetController::BroadcastInitialValue()
+{
+	Super::BroadcastInitialValue();
+}
+
+void UAttributeMenuWidgetController::BindCallbacksToDependencies()
+{
+	Super::BindCallbacksToDependencies();
+}
