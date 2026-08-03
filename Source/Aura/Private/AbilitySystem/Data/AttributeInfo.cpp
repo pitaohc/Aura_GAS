7ea95ff4e64828a18ab847fdc910fa14c99bea83
pitaohc <36119151+pitaohc@users.noreply.github.com>
2026-08-03 20:46:13 +0800

    Resolve 9-89 Attribute Info Data Asset (#202)
    Close #180

---
+++ Source/Aura/Private/AbilitySystem/Data/AttributeInfo.cpp
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "AbilitySystem/Data/AttributeInfo.h"
+
+FAuraAttributeInfo UAttributeInfo::FindAttributeInfoByTag(const FGameplayTag& Tag, bool bLogNotFound) const
+{
+	for (const auto& elem : AttributeInformation)
+	{
+		if (elem.AttributeTag == Tag)
+		{
+			return elem;
+		}
+	}
+	if (bLogNotFound)
+	{
+		UE_LOG(LogTemp,
+			Warning,
+			TEXT("Can not find Info for Attribute Tag [%s] on AttributeInfo DataAsset [%s]"),
+			*Tag.ToString(),
+			*GetName());
+	}
+	return FAuraAttributeInfo();
+}
---
+++ Source/Aura/Public/AbilitySystem/Data/AttributeInfo.h
@@ -0,0 +1,38 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Engine/DataAsset.h"
+#include "AttributeInfo.generated.h"
+
+USTRUCT(BlueprintType) // 由于添加了BlueprintReadOnly，所以需要添加BlueprintType，否则会报错
+struct FAuraAttributeInfo
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	FGameplayTag AttributeTag;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	FText AttributeName;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	FText AttributeDescription;
+	UPROPERTY(BlueprintReadOnly) // 具体的值动态读取，不需要编辑时修改
+	float AttributeValue;
+};
+
+/**
+ *
+ */
+UCLASS()
+class AURA_API UAttributeInfo : public UDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TArray<FAuraAttributeInfo> AttributeInformation;
+
+	FAuraAttributeInfo FindAttributeInfoByTag(const FGameplayTag& Tag, bool bLogNotFound = false) const;
+};
