d633e49f004603ed5c15642987db939ced2a8439
pitaohc <36119151+pitaohc@users.noreply.github.com>
2026-08-03 14:50:03 +0800

    Resolve 9-88 Native Gameplay Tags (#201)
    创建Native Gameplay Tags在FAuraGameplayTags中
    
    Close #179

--- Source/Aura/Private/AuraGameplayTags.cpp
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -1,13 +1,52 @@
 ﻿// Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "AuraGameplayTags.h"
 
 #include "GameplayTagsManager.h"
 
-
 void FAuraGameplayTags::InitializeNativeGameplayTags()
 {
-	UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Attributes.Secondary.Armor"),
-		FString("Reduces damage taken,improves Block Chance"));
+	UGameplayTagsManager& Manager = UGameplayTagsManager::Get();
+	FAuraGameplayTags&	  Tags = FAuraGameplayTags::Get();
+	// Primary
+	Tags.Attributes_Primary_Strength = Manager.AddNativeGameplayTag(
+		FName("Attributes.Primary.Strength"), FString("力量"));
+
+	Tags.Attributes_Primary_Intelligence = Manager.AddNativeGameplayTag(
+		FName("Attributes.Primary.Intelligence"), FString("智力"));
+	Tags.Attributes_Primary_Resilience = Manager.AddNativeGameplayTag(
+		FName("Attributes.Primary.Resilience"), FString("抗性"));
+	Tags.Attributes_Primary_Vigor = Manager.AddNativeGameplayTag(FName("Attributes.Primary.Vigor"), FString("活力"));
+
+	// Secondary
+	Tags.Attributes_Secondary_Armor = Manager.AddNativeGameplayTag(
+		FName("Attributes.Secondary.Armor"), FString("Reduces damage taken,improves Block Chance"));
+
+	Tags.Attributes_Secondary_ArmorPenetration = Manager.AddNativeGameplayTag(
+		FName("Attributes.Secondary.ArmorPenetration"),
+		FString("Ignores Percentage of enemy Armor, increases Critical Hit Chance"));
+
+	Tags.Attributes_Secondary_BlockChance = Manager.AddNativeGameplayTag(
+		FName("Attributes.Secondary.BlockChance"), FString("Chance to cut incoming damage in half"));
+
+	Tags.Attributes_Secondary_CriticalHitChance = Manager.AddNativeGameplayTag(
+		FName("Attributes.Secondary.CriticalHitChance"), FString("暴击概率"));
+
+	Tags.Attributes_Secondary_CriticalHitDamage = Manager.AddNativeGameplayTag(
+		FName("Attributes.Secondary.CriticalHitDamage"), FString("暴击伤害"));
+
+	Tags.Attributes_Secondary_CriticalHitResistance = Manager.AddNativeGameplayTag(
+		FName("Attributes.Secondary.CriticalHitResistance"), FString("暴击抵抗"));
+
+	Tags.Attributes_Secondary_HealthRegeneration = Manager.AddNativeGameplayTag(
+		FName("Attributes.Secondary.HealthRegeneration"), FString("血量恢复"));
+
+	Tags.Attributes_Secondary_ManaRegeneration = Manager.AddNativeGameplayTag(
+		FName("Attributes.Secondary.ManaRegeneration"), FString("法力恢复"));
+
+	Tags.Attributes_Secondary_MaxHealth = Manager.AddNativeGameplayTag(
+		FName("Attributes.Secondary.MaxHealth"), FString("血量上限"));
+
+	Tags.Attributes_Secondary_MaxMana = Manager.AddNativeGameplayTag(
+		FName("Attributes.Secondary.MaxMana"), FString("法力上限"));
 }
--- Source/Aura/Public/AuraGameplayTags.h
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -3,9 +3,10 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "NativeGameplayTags.h"
 
 /**
- * 
+ *
  */
 class FAuraGameplayTags
 {
@@ -15,12 +16,30 @@ public:
 		static FAuraGameplayTags instance;
 		return instance;
 	}
-	static void                     InitializeNativeGameplayTags();
+
+	static void InitializeNativeGameplayTags();
 	// 禁止拷贝和移动
 	FAuraGameplayTags(const FAuraGameplayTags&) = delete;
 	FAuraGameplayTags& operator=(const FAuraGameplayTags&) = delete;
 	FAuraGameplayTags(FAuraGameplayTags&&) = delete;
 	FAuraGameplayTags& operator=(FAuraGameplayTags&&) = delete;
+
+	FGameplayTag Attributes_Primary_Strength;
+	FGameplayTag Attributes_Primary_Intelligence;
+	FGameplayTag Attributes_Primary_Resilience;
+	FGameplayTag Attributes_Primary_Vigor;
+
+	FGameplayTag Attributes_Secondary_Armor;
+	FGameplayTag Attributes_Secondary_ArmorPenetration;
+	FGameplayTag Attributes_Secondary_BlockChance;
+	FGameplayTag Attributes_Secondary_CriticalHitChance;
+	FGameplayTag Attributes_Secondary_CriticalHitDamage;
+	FGameplayTag Attributes_Secondary_CriticalHitResistance;
+	FGameplayTag Attributes_Secondary_HealthRegeneration;
+	FGameplayTag Attributes_Secondary_ManaRegeneration;
+	FGameplayTag Attributes_Secondary_MaxHealth;
+	FGameplayTag Attributes_Secondary_MaxMana;
+
 private:
 	FAuraGameplayTags() = default;
 	~FAuraGameplayTags() = default;
