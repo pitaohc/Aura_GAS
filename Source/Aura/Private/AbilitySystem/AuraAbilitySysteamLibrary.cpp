6a15579cf256a13db0f47ef6f6a89e94b9772250
pitao <pitaohc@qq.com>
2026-08-04 13:03:26 +0800

    Resolve 9-91 Aura Ability System Blueprint Library

---
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySysteamLibrary.cpp
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "AbilitySystem/AuraAbilitySysteamLibrary.h"
+
+#include "AbilitySystem/AuraAbilitySystemComponent.h"
+#include "AbilitySystem/AuraAttributeSet.h"
+#include "GameFramework/GameSession.h"
+#include "Kismet/GameplayStatics.h"
+#include "Player/AuraPlayerState.h"
+#include "UI/Controller/AuraWidgetController.h"
+#include "UI/HUD/AuraHUD.h"
+
+UOverlayWidgetController* UAuraAbilitySysteamLibrary::GetOverlayWidgetController(const UObject* WorldContextObject)
+{
+	APlayerController* PC = UGameplayStatics::GetPlayerControllerFromID(WorldContextObject, 0);
+	if (!PC)
+		return nullptr;
+	AAuraHUD* AuraHUD = Cast<AAuraHUD>(PC->GetHUD());
+	if (!AuraHUD)
+		return nullptr;
+	AAuraPlayerState* PS = PC->GetPlayerState<AAuraPlayerState>();
+	if (!PS)
+		return nullptr;
+	UAbilitySystemComponent* ASC = PS->GetAbilitySystemComponent();
+	if (!ASC)
+		return nullptr;
+	UAttributeSet* AS = PS->GetAttributeSet();
+	if (!AS)
+		return nullptr;
+
+	const FWidgetControllerParams Params = { PC, PS, ASC, AS };
+	UOverlayWidgetController*	  OverlayController = AuraHUD->GetOverlayWidgetController(Params);
+	return OverlayController;
+}
