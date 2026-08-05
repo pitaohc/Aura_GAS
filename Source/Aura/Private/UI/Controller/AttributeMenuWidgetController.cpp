7d07421889cdf761060043c850ce183b9f2d39bb
pitao <pitaohc@qq.com>
2026-08-05 20:44:03 +0800

    Resolve 9-93 Attribute Info Delegate
    创建并绑定事件委托。

--- Source/Aura/Private/UI/Controller/AttributeMenuWidgetController.cpp
+++ Source/Aura/Private/UI/Controller/AttributeMenuWidgetController.cpp
@@ -2,9 +2,21 @@
 
 #include "UI/Controller/AttributeMenuWidgetController.h"
 
+#include "AuraGameplayTags.h"
+#include "AbilitySystem/AuraAttributeSet.h"
+#include "AbilitySystem/Data/AttributeInfo.h"
+
 void UAttributeMenuWidgetController::BroadcastInitialValue()
 {
 	Super::BroadcastInitialValue();
+
+	UAuraAttributeSet* AuraAttributeSet = CastChecked<UAuraAttributeSet>(AttributeSet);
+	check(AttributeInfoDataAsset)
+
+		FAuraAttributeInfo StrengthInfo = AttributeInfoDataAsset->FindAttributeInfoByTag(
+			FAuraGameplayTags::Get().Attributes_Primary_Strength);
+	StrengthInfo.AttributeValue = AuraAttributeSet->GetStrength();
+	AttributeInfoDelegate.Broadcast(StrengthInfo);
 }
 
 void UAttributeMenuWidgetController::BindCallbacksToDependencies()
