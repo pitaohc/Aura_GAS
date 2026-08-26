af214402ae46fb721b5dc86f686e8e87cbba23d7
pitaohc <36119151+pitaohc@users.noreply.github.com>
2026-08-26 21:21:59 +0800

    Resolve 10-100 Input Config Data Asset (#227)
    1. 创建 `UAuraInputConfig` 存储Action与Tag的映射关系
    2. 创建Action，配置资产
    3. 创建Input Tags
    
    Close #209

---
+++ Source/Aura/Private/Input/AuraInputConfig.cpp
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Input/AuraInputConfig.h"
+
+const UInputAction* UAuraInputConfig::GetInputActionByTag(const FGameplayTag& InputTag, bool bLogNotFound) const
+{
+	for (const auto& InputAction : InputActions)
+	{
+		if (InputAction.InputTag == InputTag)
+		{
+			return InputAction.InputAction;
+		}
+	}
+	if (bLogNotFound)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("InputAction with tag %s not found"), *InputTag.ToString());
+	}
+	return nullptr;
+}
--- Source/Aura/Public/AuraGameplayTags.h
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -23,12 +23,12 @@ public:
 	FAuraGameplayTags& operator=(const FAuraGameplayTags&) = delete;
 	FAuraGameplayTags(FAuraGameplayTags&&) = delete;
 	FAuraGameplayTags& operator=(FAuraGameplayTags&&) = delete;
-
+	// Primary Attributes
 	FGameplayTag Attributes_Primary_Strength;
 	FGameplayTag Attributes_Primary_Intelligence;
 	FGameplayTag Attributes_Primary_Resilience;
 	FGameplayTag Attributes_Primary_Vigor;
-
+	// Secondary Attributes
 	FGameplayTag Attributes_Secondary_Armor;
 	FGameplayTag Attributes_Secondary_ArmorPenetration;
 	FGameplayTag Attributes_Secondary_BlockChance;
@@ -39,6 +39,13 @@ public:
 	FGameplayTag Attributes_Secondary_ManaRegeneration;
 	FGameplayTag Attributes_Secondary_MaxHealth;
 	FGameplayTag Attributes_Secondary_MaxMana;
+	// Input Tags
+	FGameplayTag InputTag_LMB;
+	FGameplayTag InputTag_RMB;
+	FGameplayTag InputTag_1;
+	FGameplayTag InputTag_2;
+	FGameplayTag InputTag_3;
+	FGameplayTag InputTag_4;
 
 private:
 	FAuraGameplayTags() = default;
