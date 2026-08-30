191e85a2f32274e24571d96acaeb43a64804a444
pitaohc <36119151+pitaohc@users.noreply.github.com>
2026-08-30 18:08:16 +0800

    Resolve 10-110 Aura Projectile Spell (#253)
    Close #219

---
+++ Source/Aura/Private/AbilitySystem/Ability/AuraProjectileSpell.cpp
@@ -0,0 +1,14 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AbilitySystem/Ability/AuraProjectileSpell.h"
+
+void UAuraProjectileSpell::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
+	const FGameplayAbilityActorInfo* ActorInfo,
+	const FGameplayAbilityActivationInfo ActivationInfo,
+	const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+	
+	GEngine->AddOnScreenDebugMessage(-1,5,FColor::White,TEXT("UAuraProjectileSpell::ActivateAbility C++"));
+}
